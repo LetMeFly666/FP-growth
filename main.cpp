8e0421385257fa066ab8e21c58392b0053556317
LetMeFly666 <814114971@qq.com>
2022-04-10 09:55:50 +0800

    1. Init main.cpp 2. Added the icon

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2022-04-10 09:43:22
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2022-04-10 09:46:42
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
