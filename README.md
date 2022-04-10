f17bab3484b99c12024e708b9cb229e3c8d54027
LetMeFly666 <814114971@qq.com>
2022-04-10 15:52:53 +0800

    完成了初始化（分析最小支持度除外），数据读入BUG版

--- README.md
+++ README.md
@@ -2,20 +2,24 @@
  * @Author: LetMeFly
  * @Date: 2022-04-10 09:40:57
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-10 14:05:15
+ * @LastEditTime: 2022-04-10 15:03:21
 -->
 # FP-growth
 
 FP-growth算法实现关联规则的数据挖掘
 
-See [Apriori(https://github.com/LetMeFly666/Apriori)](https://github.com/LetMeFly666/Apriori) For More
++ 在线文档 [https://fp-growth.letmefly.xyz](https://fp-growth.letmefly.xyz)
+
++ 项目地址 [https://github.com/LetMeFly666/FP-growth](https://github.com/LetMeFly666/FP-growth)
+
++ See [Apriori(https://github.com/LetMeFly666/Apriori)](https://github.com/LetMeFly666/Apriori) For More
 
 本次决定先实现代码再进行优化，否则直接开始想具体哪种最优的话太慢了。
 
 ## 使用方法
 
 ```bash
-FP-growth[.exe] [-i DATANAME] [-s SUPPORT] [-o RESULT]
+FP-growth[.exe] [-i DATANAME] [-s SUPPORT] [-o RESULT] [-h]
 ```
 
 其中```[]```中的内容表示可选。
@@ -26,6 +30,8 @@ FP-growth[.exe] [-i DATANAME] [-s SUPPORT] [-o RESULT]
 | :--: |:--: |:--: |:--: |
 | ```-i``` | ✖ | 输入文件名 | 如果参数中有```-i```，那么程序将会以此参数的下一个参数作为输入文件。如```FP-growth -i source/retail.dat``` |
 | ```-s``` | ✖ | 最小支持度 | 如果参数中有```-s```，那么程序将会以此参数的下一个参数作为[最小支持度(百分比)](#minSupport)。如```FP-growth -s 1.25%``` |
+| ```-o``` | ✖ | 重定向输出 | 如果参数中有```-o```，那么程序将会重定向输出到下一个参数所代表的文件中。如```FP-growth -o output.txt```。相当于```FP-growth > output.txt```。提示性信息如"请输入数据文件名"不会重定向到该指定文件中。 |
+| ```-h``` | ✖ | 显示帮助 | 如果参数中有```-h```，那么程序将会打开在线帮助文档[https://fp-growth.letmefly.xyz](https://fp-growth.letmefly.xyz)。 |
 
 #### 最小支持度 <a id="minSupport"></a>
 
@@ -42,3 +48,4 @@ FP-growth[.exe] [-i DATANAME] [-s SUPPORT] [-o RESULT]
 | :--: | :--: | :--: |
 | ```0``` | 程序无误 | 正确运行 |
 | ```1``` | 参数不足 | 如```-i```后需要参数```DATANAME```，但若```-i```后没有此参数将会返回此错误 |
+| ```2``` | 无法打开文件 | 无法读入输入文件，请检查输入文件是否存在 |
--- main.cpp
+++ main.cpp
@@ -2,22 +2,124 @@
  * @Author: LetMeFly
  * @Date: 2022-04-10 09:43:22
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-10 13:59:13
+ * @LastEditTime: 2022-04-10 15:51:21
  */
+#include <windows.h>  // Sleep
 #include <iostream>
+#include <fstream>
+#include <istream>  // getline
+#include <cstring>
+#include <vector>
 using namespace std;
 #define dbg(x) cout << #x << " = " << x << endl
 
-char* dataName;
-double minSupportPercent;
-int minSupportNum = 0;
+#define EXIT1IFNOANOTHERPARAMETR if (i + 1 >= argc) exit(1)  // 如果参数个数不足就exit(1)
+#define SlowExit(toSay, errCode) {cerr << toSay << endl; Sleep(500); exit(errCode);}
 
-void init(int agrc, char** argv) {
-    
+
+string dataName;  // 输入文件名
+int minSupportNum = 0;  // 最小支持度
+vector<vector<int>> database;  // 数据库(内存版本)
+
+
+void init(int argc, char** argv);
+void input();
+void debug_input();
+void analyMinSupportNum(string minSupportInput);
+
+
+/**
+ * 1. 分析命令行参数，未传递参数提示输入
+ * 2. 初始化读入数据
+ * 3. 确定最小支持度
+ */
+void init(int argc, char** argv) {
+    string minSupportInput;
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-i")) {
+            EXIT1IFNOANOTHERPARAMETR;
+            dataName = argv[++i];
+        }
+        else if (!strcmp(argv[i], "-s")) {
+            EXIT1IFNOANOTHERPARAMETR;
+            minSupportInput = argv[++i];
+        }
+        else if (!strcmp(argv[i], "-o")) {
+            EXIT1IFNOANOTHERPARAMETR;
+            freopen(argv[++i], "w", stdout);  // 输出到文件
+        }
+        else if (!strcmp(argv[i], "-h")) {
+            system("start https://fp-growth.letmefly.xyz");
+        }
+    }
+    if (dataName.empty()) {  // 输入文件
+        cerr << "Please input the file name of the database: ";
+        cin >> dataName;
+    }
+    if (minSupportInput.empty()) {  // 最小支持度
+        cerr << "Please input the min support: ";
+        cin >> minSupportInput;
+    }
+    input();
+    analyMinSupportNum(minSupportInput);
+}
+
+/* 将dataName中的数据读入到database中 */
+void input() {  // 读入数据
+    ifstream istr(dataName.c_str(), ios::in);
+    if (istr.fail()) {
+        SlowExit("Open input file failed.", 2);
+    }
+    string line;
+    int cnt = 0;
+    while (getline(istr, line)) {
+        vector<int> thisLog;
+        bool lastIsNum = false;
+        int num = 0;
+        for (char& c : line) {
+            if (c >= '0' && c <= '9') {
+                num *= 10;
+                num += c - '0';
+                lastIsNum = true;
+            }
+            else {
+                if (lastIsNum) {
+                    thisLog.push_back(num);
+                    num = 0;
+                }
+                lastIsNum = false;
+            }
+        }
+        database.push_back(thisLog);
+    }
+    istr.close();
+}
+
+/* 将输入的最小支持度(string)转为最小支持数(int) */
+void analyMinSupportNum(string minSupportInput) {
+
+}
+
+/* Debug: 输出database(vector<vector<int>>) */
+void debug_input() {
+    puts("database:");
+    puts("------------------");
+    for (vector<int>& thisLog : database) {
+        bool first = true;
+        for (int& t : thisLog) {
+            if (first)
+                first = false;
+            else
+                putchar(' ');
+            cout << t;
+        }
+        puts("");
+    }
+    puts("------------------");
 }
 
 int main(int argc, char** argv) {
     init(argc, argv);
-    
+    debug_input();
     return 0;
 }
