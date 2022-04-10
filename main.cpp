fe6d10c6f42a7a62159bae94f4f5695d8ce04e04
LetMeFly666 <814114971@qq.com>
2022-04-10 14:05:39 +0800

    （中午吃饭太久了） 1. 决定先实现再优化 2. 参数列表 3. 错误类型列表 4. ignore了*.exe

--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
  * @Author: LetMeFly
  * @Date: 2022-04-10 09:43:22
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-10 09:46:42
+ * @LastEditTime: 2022-04-10 13:59:13
  */
 #include <iostream>
 using namespace std;
+#define dbg(x) cout << #x << " = " << x << endl
 
-int main() {
+char* dataName;
+double minSupportPercent;
+int minSupportNum = 0;
+
+void init(int agrc, char** argv) {
+    
+}
+
+int main(int argc, char** argv) {
+    init(argc, argv);
     
     return 0;
 }
